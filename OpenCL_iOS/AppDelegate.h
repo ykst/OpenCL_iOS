dc861051acad6c6caafc8626cf9dd0546f8bd1c9
Yohsuke Yukishita <ykstyhsk@gmail.com>
2014-12-06 19:03:36 +0900

    Initial Commit

---
+++ OpenCL_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCL_iOS
+//
+//  Created by Yukishita Yohsuke on H26/12/06.
+//  Copyright (c) 平成26年 monadworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
